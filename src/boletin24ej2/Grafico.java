af71f6cceb8e21761d95de9a678e34d5e855b48e
bhernandezsouto <bhernandezsouto@atanasoft04.danielcastelao.org>
2015-04-10 12:38:21 +0200

    wtrertert

--- src/boletin24ej2/Grafico.java
+++ src/boletin24ej2/Grafico.java
@@ -1,6 +1,5 @@
 package boletin24ej2;
 
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -8,31 +7,36 @@ import javax.swing.JPanel;
 
 /*
  * 2- Crea unha aplicación con  un marco que ten de título “ CURSO DAM “ e 2 botons :
-programación e outro bases . Se prememos o botón programación  o titulo do JFrame debe
-ser  programación  e se prememos bases debe ser bases de datos.
+ programación e outro bases . Se prememos o botón programación  o titulo do JFrame debe
+ ser  programación  e se prememos bases debe ser bases de datos.
  */
-
 /**
  *
  * @author bhernandezsouto
  */
-public class Grafico{
+public class Grafico extends javax.swing.JFrame {
+
     JFrame marco;
     JPanel panel;
     JButton boton1, boton2;
-        public void formar(){
+
+    public void formar() {
         marco = new JFrame();
         panel = new JPanel();
         boton1 = new JButton("Bases");
         boton2 = new JButton("Programación");
         marco.setVisible(true);
         panel.setVisible(true);
-    }
-    public void pulsarBases(java.awt.event.MouseEvent evt){
-        marco.setName("Bases.");
-    }
-     public void pulsarProg(java.awt.event.MouseEvent evt){
-        marco.setName("Programación.");
-    }
-}
+        boton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void botonPulsado(java.awt.event.MouseListener evt) {
+                marco.setName("Bases.");
+            }
+        });
 
+        boton2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void botonPulsado(java.awt.event.MouseEvent evt) {
+                marco.setName("Programación.");
+            }
+        });
+
+    }
